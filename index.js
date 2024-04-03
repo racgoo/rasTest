878b6374f183e854e33187972b0f02290404119c
racgoo <lhsung98@naver.com>
2024-04-03 13:40:12 +0900

    test

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Gpio = require('pigpio').Gpio;
-
-const led = new Gpio(17, {mode: Gpio.OUTPUT});
-
+const LC = require("libcamera").libcamera; 
+const led_B = new Gpio(17, {mode: Gpio.OUTPUT});
+const led_Y = new Gpio(27, {mode: Gpio.OUTPUT});
 let dutyCycle = 0;
-
+LC.jpeg({config: {output: "test.jpg"}});
 setInterval(() => {
-  led.pwmWrite(dutyCycle);
-
+  led_B.pwmWrite(dutyCycle);
+  led_Y.pwmWrite(dutyCycle);
   dutyCycle += 5;
   if (dutyCycle > 255) {
     dutyCycle = 0;
